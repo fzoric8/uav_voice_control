d65dd348f5d81aaf3f0b7b35f0fcdf0b34ca8303
karajfilip <filip.karaj@gmail.com>
2021-04-16 23:39:15 +0200

    second commit

--- src/voice_rec.py
+++ src/voice_rec.py
@@ -1,46 +1,25 @@
 #!/usr/bin/env python3
 
 import rospy
-import pyaudio
-from os import environ, path
 from pocketsphinx import LiveSpeech
-from sphinxbase import *
 from std_msgs.msg import String
 
 
 def voice_recog(voice):
-    #if not rospy.is_shutdown():
     voice_rec = String()
     voice_rec.data = str(voice)
-    print("checkpoint")
     print(voice_rec)
-    print(voice_rec.data)
     pub.publish(voice_rec.data)
-    #rospy.sleep(1.0)
-		#rospy.spin()
 
 
 if __name__ == '__main__':
     pub = rospy.Publisher('voice_recognition', String, queue_size=10)
     rospy.init_node('voice_publish')
-    #speech = LiveSpeech(lm=False, keyphrase='up', kws_threshold=1e-20)
-    #speech1 = LiveSpeech(lm=False, keyphrase='up', kws_threshold=1e-10)
+    speech = LiveSpeech(lm=False,
+                        kws='/home/filip/catkin_ws/src/uav_voice_control/src/key.list', verbose=False,
+                        no_search=False, full_utt=False, buffer_size=2048, sampling_rate=16000)
 
-#for phrase in speech:
     while not rospy.is_shutdown():
-        for phrase in LiveSpeech():
-            v = phrase
-            print(type(phrase))
-            print(dir(phrase))
-        #v = phrase.segments(detailed=True)
-            print(v)
-            voice_recog(v)
+        for phrase in speech:
+            voice_recog(phrase)
             rospy.sleep(1.0)
-
-
-
-        #try:
-            #while not rospy.is_shutdown():
-                #voice_recog(v)
-        #except rospy.ROSInterruptException:
-            #print("palo")
