44673396929ff083ae9e0efae13049198795813c
Filip Zoric <fzoric8@gmail.com>
2021-05-04 16:41:26 +0200

    Adding some minor changes for audio device detection

--- src/voice_rec.py
+++ src/voice_rec.py
@@ -1,6 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import rospy
+import pyaudio 
 from pocketsphinx import LiveSpeech
 from std_msgs.msg import String
 
@@ -15,9 +16,22 @@ def voice_recog(voice):
 if __name__ == '__main__':
     pub = rospy.Publisher('voice_recognition', String, queue_size=10)
     rospy.init_node('voice_publish')
+    
+    # Check which microphone to use 
+    p = pyaudio.PyAudio()
+    print("===================================================")
+    for i in range(p.get_device_count()):
+        info = p.get_device_info_by_index(i)
+        print("Device index {} name is: {}".format(i, info["name"]))
+        print("Device info by index: ", info)
+    
+    # Instantiate LiveSpeech class 
     speech = LiveSpeech(lm=False,
-                        kws='/home/filip/catkin_ws/src/uav_voice_control/src/key.list', verbose=False,
-                        no_search=False, full_utt=False, buffer_size=2048, sampling_rate=16000)
+			# Set relative path here for key.list -> should work on any PC
+                        kws='/home/developer/catkin_ws/src/uav_voice_control/src/key.list', verbose=False,
+                        no_search=False, full_utt=False, buffer_size=2048, sampling_rate=16000, audio_device="dmix")
+			# If you don't pass any argument while creating an instance of Pockesphinx, AudioFile or 
+                        # LiveSpeech class, it will use next default values: https://github.com/bambocher/pocketsphinx-python#default-config
 
     while not rospy.is_shutdown():
         for phrase in speech:
