56ed05ed40c0d2fc416fb27ffb134fcf5cd442bb
karajfilip <filip.karaj@gmail.com>
2021-05-13 23:40:46 +0200

    added left, right, forward, backward

--- src/uav_control.py
+++ src/uav_control.py
@@ -36,6 +36,15 @@ class UAV_Control():
         self.detected_word = str(msg.data)
         print(self.detected_word)
 
+        if 'takeoff' in self.detected_word:
+            if self.position.z == 0:
+                self.position.z = 0.5
+                time.sleep(3)
+                self.position.z = 1
+                print('uav_control: take_off')
+            else:
+                print('uav: airborne')
+
         if 'land' in self.detected_word:
             if self.position.z >= 2:
                 self.position.z = 1
@@ -74,6 +83,63 @@ class UAV_Control():
                 print("Penjem se 100cm")
             self.orientation.w = 1
 
+        if 'right' in self.detected_word:
+            if 'one' in self.detected_word:
+                self.position.x += 0.1
+                print("Idem desno za 10cm")
+            elif 'two' in self.detected_word:
+                self.position.x += 0.2
+                print("Idem desno za 20cm")
+            elif 'three' in self.detected_word:
+                self.position.x += 0.3
+                print("Idem desno za 30cm")
+            elif 'ten' in self.detected_word:
+                self.position.x += 1
+                print("Idem desno za 100cm")
+
+        if 'left' in self.detected_word:
+            if 'one' in self.detected_word:
+                self.position.x -= 0.1
+                print("Idem lijevo za 10cm")
+            elif 'two' in self.detected_word:
+                self.position.x -= 0.2
+                print("Idem lijevo za 20cm")
+            elif 'three' in self.detected_word:
+                self.position.x -= 0.3
+                print("Idem lijevo za 30cm")
+            elif 'ten' in self.detected_word:
+                self.position.x -= 1
+                print("Idem lijevo za 100cm")
+
+        if 'forward' in self.detected_word:
+            if 'one' in self.detected_word:
+                self.position.y += 0.1
+                print("Idem naprijed za 10cm")
+            elif 'two' in self.detected_word:
+                self.position.y += 0.2
+                print("Idem naprijed za 20cm")
+            elif 'three' in self.detected_word:
+                self.position.y += 0.3
+                print("Idem naprijed za 30cm")
+            elif 'ten' in self.detected_word:
+                self.position.y += 1
+                print("Idem naprijed za 100cm")
+
+        if 'backward' in self.detected_word:
+            if 'one' in self.detected_word:
+                self.position.y -= 0.1
+                print("Idem naprijed za 10cm")
+            elif 'two' in self.detected_word:
+                self.position.y -= 0.2
+                print("Idem naprijed za 20cm")
+            elif 'three' in self.detected_word:
+                self.position.y -= 0.3
+                print("Idem naprijed za 30cm")
+            elif 'ten' in self.detected_word:
+                self.position.y -= 1
+                print("Idem naprijed za 100cm")
+
+        """
         if self.detected_word == 'stop':
             self.position.x = 0
             self.position.y = 0
@@ -83,25 +149,7 @@ class UAV_Control():
             self.orientation.z = 0
             self.orientation.w = 0
             print('uav_control:stop')
-
-        if 'forward' in self.detected_word:
-            self.position.x = 1
-            self.position.y = 1
-            self.position.z = 1
-            self.orientation.x = 1
-            self.orientation.y = 1
-            self.orientation.z = 1
-            self.orientation.w = 1
-            print('uav_control:forward')
-
-        if 'takeoff' in self.detected_word:
-            if self.position.z == 0:
-                self.position.z = 0.5
-                time.sleep(3)
-                self.position.z = 1
-                print('uav_control:take_off')
-            else:
-                print('uav:airborne')
+        """
 
     def run(self):
         while not rospy.is_shutdown():
@@ -113,8 +161,3 @@ if __name__ == '__main__':
     rospy.init_node('uav_control')
     uc = UAV_Control()
     uc.run()
-
-
-# TODO subscribat na odometriju - uzeti poziciju s odometrije i poslati pola metra
-# land -> na 10 cm i onda na nulu
-# home - (0,0, 1)
