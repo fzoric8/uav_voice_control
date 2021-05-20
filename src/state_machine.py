dafb157dd8be78f027f35f9d4ebd9a7f781e3eb2
karajfilip <filip.karaj@gmail.com>
2021-05-20 16:49:45 +0200

    sm with service and client

--- src/state_machine.py
+++ src/state_machine.py
@@ -6,49 +6,73 @@ import smach
 import smach_ros
 from std_msgs.msg import String
 from uav_control import UAV_Control
+from uav_voice_control.srv import ProvideMove, ProvideMoveResponse
 # from voice_rec import received_word
+
+r_word = False
 
 class Ready(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes = ['outcome1', 'outcome2'])
+        smach.State.__init__(self, outcomes=['outcome1', 'outcome2'])
         rospy.Subscriber('/voice_recognition', String, self.received_word)
-        self.r_word = False
+        global r_word
 
     def received_word(self, msg):
-        self.r_word = True
+        global r_word
+        r_word = True
 
     def execute(self, userdata):
         rospy.loginfo('State: Ready')
-        if self.r_word == True:
+        rospy.sleep(1)
+        if r_word == True:
             return 'outcome2'
         else:
             return 'outcome1'
 
 class Moving(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes = ['outcome1'])
+        smach.State.__init__(self, outcomes=['outcome1', 'outcome2'])
+        global r_word
 
-    def execute(self):
+    def execute(self, userdata):
         rospy.loginfo('State: Moving')
-        uc = UAV_Control()
-        UAV_Control.run(uc)
+        rospy.sleep(1)
+        # self.r_word = False
 
-        return 'outcome1'
+        print('1')
+        rospy.wait_for_service('/uav/provide_move')
+        print('2')
+        try:
+            self.resp1 = ProvideMoveResponse()
+            provide_move = rospy.ServiceProxy('/uav/provide_move', ProvideMove)
+            self.resp1 = provide_move(r_word)
+
+        except rospy.ServiceException as exc:
+            print("Service did not process request: " + str(exc))
+
+        if self.resp1 == True:
+            return 'outcome1'
+        else:
+            return 'outcome2'
+
 
 def main():
     rospy.init_node('state_machine')
 
     # Create a SMACH state machine
-    sm = smach.StateMachine(outcomes = ['outcome1'])
+    sm = smach.StateMachine(outcomes=['outcome1'])
 
     # Open the container
     with sm:
 
         # Add states to the container
-        smach.StateMachine.add('READY', Ready(), transitions = {'outcome1':'READY', 'outcome2':'MOVING'})
-        smach.StateMachine.add('MOVING', Moving(), transitions = {'outcome1':'READY'})
+        smach.StateMachine.add('READY', Ready(), transitions={'outcome1': 'READY', 'outcome2': 'MOVING'})
+        smach.StateMachine.add('MOVING', Moving(), transitions={'outcome1': 'READY', 'outcome2': 'MOVING'})
 
     outcome = sm.execute()
+    uc = UAV_Control()
+    UAV_Control.run(uc)
+
 
 if __name__ == '__main__':
     main()
